9d56b31042c2a01b011b00ecc8be3a2bafe1fa05
Fabio Gomez <fabiote2003@gmail.com>
2022-11-29 18:32:24 -0300

    first commit

---
+++ ejercicio-1.js
@@ -0,0 +1,236 @@
+//Ejercicio 1
+
+
+// This is an in-line comment.
+/* This 
+is a multi-line comment
+*/
+
+
+//Ejercicio 2
+var myName;
+
+
+//Ejercicio 3
+
+var a;
+a = 7
+
+//Ejercicio 4
+
+var a;
+a = 7;
+var b;
+b = a;
+
+//Ejercicio 5
+var a = 9
+
+
+//Ejercicio 6
+var myFirstName = "Fabio"
+var myLastName  = "Gomez"
+
+
+//Ejercicio 7
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+
+
+//Ejercicio 8
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+//Ejercicio 9
+let catName = "Oliver";
+let catSound = "Meow!";
+
+
+//Ejercicio 10
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+//Ejercicio 11
+const sum = 10 + 10;
+
+
+//Ejercicio 12
+const difference = 45 - 33;
+
+
+//Ejercicio 13
+const product = 8 * 10;
+
+//Ejercicio 14
+const quotient = 66 / 33;
+
+
+
+//Ejercicio 15
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+
+//Ejercicio 16
+let myVarNew = 11;
+
+// Only change code below this line
+myVarNew--;
+
+
+//Ejercicio 17
+const ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 3.23454;
+
+
+//Ejercicio 18
+const producto = 2.0 * 2.5;
+
+
+//Ejercicio 19
+const cociente = 4.4 / 2.0; // Change this line
+
+
+//Ejercicio 20
+const remainder = 11 % 3;
+
+
+//Ejercicio 21
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+
+//Ejercicio 22
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -=  6;
+b -=  15;
+c -=  1;
+
+
+//Ejercicio 23
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+//Ejercicio 24
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+//Ejercicio 25
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+
+
+//Ejercicio 26
+let myString = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Ejercicio 27
+var myString2 = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//Ejercicio 28
+const myString3 = "This is the start." + " " + "This is the end.";
+
+//Ejercicio 29
+let myString4 = "This is the first sentence.";
+myString4 += " " 
+myString4 += "This is the Second sentence.";
+
+
+//Ejercicio 30
+var myStr = "This is the first sentence. "
+myStr += "This is the second sentence."
+
+
+//Ejercicio 31
+const myName = "Fabio";
+const myStr = "My name is " + myName + " and I am well";
+
+//Ejercicio 32
+var someAdjective = "worthwhile";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+
+
+//Ejercicio 33
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+
+//Ejercicio 34
+let firstLetterOfLastName = "Gomez";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+
+//Ejercicio 35
+var myStr = "Jello World";
+
+myStr = "Hello World";
+
+//Ejercicio 36
+
+const lastName = "Lovelace";
+
+var thirdLetterOfLastName = lastName[2];
+
+//Ejercicio 37
+
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+
+//Ejercicio 38
+const lastName = "Lovelace";
+
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+//Ejercicio 39
+
+//Ejercicio 40
+
+
+
